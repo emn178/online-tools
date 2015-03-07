386e0e7f72edc43f283aae7c4169db686d37b1a3
Chen Yi-Cyuan <emn178@gmail.com>
2015-03-07 11:24:30 +0800

    add sha1 checksum

--- app/contents/js/main.js
+++ app/contents/js/main.js
@@ -4,9 +4,10 @@
   $(document).ready(function() {
     var input = $('#input');
     var output = $('#output');
-    var checkbox = $('#auto_update');
+    var checkbox = $('#auto-update');
+    var dropzone = $('#droppable-zone');
 
-    function execute() {
+    var execute = function() {
       try {
         output.val(method(input.val()));
       } catch(e) {
@@ -21,10 +22,60 @@
       execute();
     }
 
-    $('#execute').click(execute);
     if(checkbox.length > 0) {
       input.bind('input propertychange', autoUpdate);
       checkbox.click(autoUpdate);
     }
+
+    if(dropzone.length > 0) {
+      var dropzonetext = $('#droppable-zone-text');
+
+      $(document.body).bind('dragover drop', function(e) {
+        e.preventDefault();
+        return false;
+      });
+
+      if(!window.FileReader) {
+        dropzonetext.text('Your browser dose not support.');
+        $('input').attr('disabled', true);
+        return;
+      }
+
+      dropzone.bind('dragover', function() {
+        dropzone.addClass('hover');
+      });
+
+      dropzone.bind('dragleave', function() {
+        dropzone.removeClass('hover');
+      });
+
+      dropzone.bind('drop', function(e) {
+        dropzone.removeClass('hover');
+        file = e.originalEvent.dataTransfer.files[0];
+        dropzonetext.text(file.name);
+        autoUpdate();
+      });
+
+      input.bind('change', function() {
+        file = input[0].files[0];
+        dropzonetext.text(file.name);
+        autoUpdate();
+      });
+
+      var file;
+      execute = function() {
+        reader = new FileReader();
+        reader.onload = function (event) {
+          try {
+            output.val(method(event.target.result));
+          } catch(e) {
+            output.val(e);
+          }
+        };
+        reader.readAsArrayBuffer(file);
+      };
+    }
+
+    $('#execute').click(execute);
   });
 })(jQuery, window, document);
---
+++ app/contents/sha1_checksum.md
@@ -0,0 +1,11 @@
+---
+title: SHA1 File Checksum
+template: page.jade
+js: https://rawgit.com/emn178/js-sha1/master/build/sha1.min.js
+method: sha1
+action: Hash
+auto_update: true
+file_input: true
+description: SHA1 online hash file checksum function
+keywords: SHA1,online,hash,checksum
+---
