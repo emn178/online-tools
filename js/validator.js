6c44abd8861288c6e7ac3a6534ea96c7dd1f9174
Yi-Cyuan Chen <emn178@gmail.com>
2024-08-07 09:42:26 +0800

    Site updated: 2024-08-07 09:42:26

---
+++ js/validator.js
@@ -0,0 +1,1 @@
+!function(){var a=$("#validate-result"),t=ot.setOutput;ot.setSetOutput(function(l,d){t(l,d),"Valid"===l.val()?(a.addClass("valid").removeClass("invalid").text("Valid"),l.val("")):a.addClass("invalid").removeClass("valid").text("Invalid")})}();
