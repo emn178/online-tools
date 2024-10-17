243e1a2bc2ef02160a558c7820ff803eb486b84b
Yi-Cyuan Chen <emn178@gmail.com>
2024-10-17 10:08:01 +0800

    Site updated: 2024-10-17 10:08:01

---
+++ js/file-loader.js
@@ -0,0 +1,1 @@
+!function(){function e(){"text"!==a.val()&&(a.off("change",e),waitLoadCount+=3,ot.createOnDemandScript("js/url-blob.js?v=1").load(methodLoad),ot.createOnDemandScript("js/droppable-file.js").load(methodLoad),ot.createOnDemandScript("js/file.js?v=10").load(methodLoad))}var a=$("#file-type");a.change(e),e()}();
