6d188c6b8b41cd80bf42eeceff629ffc2b29e672
Yi-Cyuan Chen <emn178@gmail.com>
2026-07-03 11:28:37 +0800

    Site updated: 2026-07-03 11:28:37

--- js/download.js
+++
@@ -1,1 +0,0 @@
-!function(){var t=$("#execute"),e=$("#download-file-name"),a=ot.getInput;ot.setGetInput(function(o){return t.removeAttr("href"),t.attr("download",e.val()),a(o)}),ot.setSetOutput(function(e,a){t.attr("href","data:application/octet-stream;base64,"+a),e.val("The download should have started.")})}();
